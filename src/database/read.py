e692e4d1f4b34c5a9d1f195237fa63f5f4e26597
Aaron <aagavin@gmail.com>
2022-02-10 23:01:38 -0500

    updated ImdbParser to get more info

--- src/database/read.py
+++ src/database/read.py
@@ -4,26 +4,46 @@ from html.parser import HTMLParser
 
 from . import models
 
+
 class ImdbParser(HTMLParser):
 
-        def __init__(self) -> None:
-            super().__init__()
-            self.reset()
-            self.data = ''
+    def __init__(self) -> None:
+        super().__init__()
+        self.reset()
+        self.posterUrl = ''
+        self.isDescription = False
+        self.description = ''
+        self.isNextEp = False
+        self.nextEp = ''
 
-        def handle_starttag(self, tag, attrs):
-            if tag == 'img' and self.data == '':
-                    for attr in attrs:
-                            if attr[0] == 'src' and attr[1].startswith('https://m.media-amazon.com/images'):
-                                    self.data = attr[1]
+    def handle_starttag(self, tag, attrs):
+        if tag == 'img' and self.posterUrl == '':
+            for attr in attrs:
+                if attr[0] == 'src' and attr[1].startswith('https://m.media-amazon.com/images'):
+                    self.posterUrl = attr[1]
+        if tag == 'span' and self.description == '':
+            for attr in attrs:
+                if attr[0] == 'data-testid' and attr[1].startswith('plot-xl'):
+                    self.isDescription = True
+        if tag == 'div' and self.nextEp == '':
+            for attr in attrs:
+                if attr[0] == 'class':
+                    print(attr[1])
+                    # self.isNextEp = True
 
+    def handle_data(self, text: str) -> None:
+        if self.isDescription:
+            self.description = text
+            self.isDescription = False
+        if self.isNextEp:
+            self.nextEp = text
+            self.isNextEp = False
 
 
 def get_title_basic(db: Session, full: bool, tconst: str):
     if full:
-        res = db.query(models.TitleAkas).filter(models.TitleAkas.titleId == tconst).order_by(models.TitleAkas.ordering.asc()).all()
-    res = db.query(models.TitleBasic).filter(models.TitleBasic.tconst == tconst).first()
-    return res
+        return db.query(models.TitleAkas).filter(models.TitleAkas.titleId == tconst).order_by(models.TitleAkas.ordering.asc()).all()
+    return db.query(models.TitleBasic).filter(models.TitleBasic.tconst == tconst).first()
 
 
 def get_title_crew(db: Session, tconst: str):
@@ -33,13 +53,16 @@ def get_title_crew(db: Session, tconst: str):
 def get_title_principals(db: Session, tconst: str):
     return db.query(models.TitlePrincipals).filter(models.TitlePrincipals.tconst == tconst).all()
 
+
 def get_title_ratings(db: Session, tconst: str):
     return db.query(models.TitleRatings).filter(models.TitleRatings.tconst == tconst).first()
+
 
 def get_title_episodes(db: Session, tconst: str):
     return db.query(models.TitleEpisode).filter(models.TitleEpisode.parentTconst == tconst).order_by(models.TitleEpisode.seasonNumber).all()
 
-def get_name_basic(db: Session, nconst: Union[list,str]):
+
+def get_name_basic(db: Session, nconst: Union[list, str]):
     if isinstance(nconst, list):
         return db.query(models.NameBasics).filter(models.NameBasics.nconst.in_(nconst)).all()
     return db.query(models.NameBasics).filter(models.NameBasics.nconst == nconst).first()
